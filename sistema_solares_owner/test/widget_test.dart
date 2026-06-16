0846a45b51c93bd0d74da900c503b1333331fd3c
JUNIORPRUEVA <JUNIORPRUEVA@users.noreply.github.com>
2026-06-16 17:15:20 -0400

    sdasdasdasdasd

--- sistema_solares_owner/test/widget_test.dart
+++ sistema_solares_owner/test/widget_test.dart
@@ -2,8 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:sistema_solares_owner/main.dart';
 
 void main() {
-  testWidgets('owner app renders title', (tester) async {
+  testWidgets('owner app renders dashboard', (tester) async {
     await tester.pumpWidget(const OwnerApp());
-    expect(find.text('Sistema Solares Owner'), findsOneWidget);
+    expect(find.text('Resumen'), findsOneWidget);
   });
 }
